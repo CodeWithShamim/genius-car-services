3c8b0585457186ca61b07672c376d1cf06d4d6e9
codewithshamim <shamimislamonline@gmail.com>
2022-04-15 15:51:23 +0600

    service details component added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Another Repo link------------
+# Go main repo --->>
 
-[https://github.com/CodeWithShamim/genius-car-services](https://github.com/CodeWithShamim/ML-10-React-Authentication/tree/main/Module%2060%20(%20Genius%20car%20services%20)/genius-car-services)
+[https://github.com/CodeWithShamim/ML-10-React-Authentication](https://github.com/CodeWithShamim/ML-10-React-Authentication/tree/main/Module%2060%20(%20Genius%20car%20services%20)/genius-car-services)
--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Pages/Shared/Header";
 import Home from "./Pages/Home/Home/Home";
 import Footer from "./Pages/Shared/Footer";
-import Services from "./Pages/Home/Services/Services";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./Pages/Shared/NotFound";
+import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
 
 function App() {
   return (
@@ -12,7 +13,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/" element={<Services></Services>}></Route>
+        <Route
+          path="service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        ></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
