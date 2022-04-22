017cff2061a2124a9d4db227568b4185469a46a8
codewithshamim <shamimislamonline@gmail.com>
2022-04-22 19:32:56 +0600

    get data from database

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 import "./Service.css";
 
 const Service = ({ service }) => {
-  const { id, name, img, description, price } = service;
+  const { _id, name, img, description, price } = service;
 
   const navigate = useNavigate();
-  const handleServiceDetail = (id) => {
-    navigate(`/service/${id}`);
+  const handleServiceDetail = (_id) => {
+    navigate(`/service/${_id}`);
   };
   return (
     <div className="service-container">
@@ -17,7 +17,7 @@ const Service = ({ service }) => {
       <h5>Price: {price}</h5>
       <button
         className="btn btn-primary"
-        onClick={() => handleServiceDetail(id)}
+        onClick={() => handleServiceDetail(_id)}
       >
         Booking-{name}
       </button>
