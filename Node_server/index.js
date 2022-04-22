98199ed68ee71f8d16b067a2654829674f40ef1b
codewithshamim <shamimislamonline@gmail.com>
2022-04-22 19:31:53 +0600

    get data from database

--- Node_server/index.js
+++ Node_server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +8,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// get 
-app.get('/', (req, res) => {
-    res.send("success");
-});
+
+
+const uri = "mongodb+srv://userdb:userdb100@cluster0.41efc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db('geniusCar').collection('service');
+        const query = {};
+        // get 
+        app.get('/services', async(req, res) => {
+            const cursor = userCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+    } finally {
+        // await client.close();
+
+    }
+}
+run().catch(console.dir)
+
+
 
 app.listen(port, () => {
     console.log("Listening to port", port)
