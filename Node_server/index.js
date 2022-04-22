769d1a6cb39b76095509a0b726d080eb92ed8700
codewithshamim <shamimislamonline@gmail.com>
2022-04-22 22:25:22 +0600

    create server for get single data from database

--- Node_server/index.js
+++ Node_server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -24,6 +24,14 @@ async function run() {
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        });
+        // get single data
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query)
+            const result = await serviceCollection.findOne(query);
             res.send(result);
         });
         // post 
