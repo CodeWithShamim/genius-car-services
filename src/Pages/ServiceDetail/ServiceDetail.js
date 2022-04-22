697f46f767da30f7bd08e74d627f2712c56441fa
codewithshamim <shamimislamonline@gmail.com>
2022-04-22 22:26:58 +0600

    successfully get single data from db

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
+  const [service, setService] = useState({});
+
+  useEffect(() => {
+    const url = `http://localhost:5000/services/${serviceId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
 
   return (
     <div>
-      <h1>Service Detail Id: {serviceId}</h1>
+      <h1>Service Detail Id: {service.name}</h1>
       <Link to="/checkout" className="btn btn-primary">
         Check Out
       </Link>
