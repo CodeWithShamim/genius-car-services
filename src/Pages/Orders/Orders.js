f24fa17db74d68929b38c817e7bcec3f8b119527
codewithshamim <shamimislamonline@gmail.com>
2022-04-28 12:40:41 +0600

    Get order info per user

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,14 +1,31 @@
-import React from "react";
+import { async } from "@firebase/util";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 import DynamicTitle from "../Shared/DynamicTitle";
 
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      const email = user.email;
+      const url = `http://localhost:5000/orders?email=${email}`;
+      const { data } = await axios.get(url);
+      setOrders(data);
+    };
+    getOrders();
+  }, []);
+
   return (
     <div
       style={{ minHeight: "60vh" }}
       className="d-flex flex flex-column align-items-center justify-content-center"
     >
       <DynamicTitle title={"checkout"}></DynamicTitle>
-      <h1>this is orders</h1>
+      <h1>this is orders : {orders?.length}</h1>
     </div>
   );
 };
